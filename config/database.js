ca38a3d788d64e5493c31a56c9b7def7b1773530
LIM DONG JOO <dongdongalcohol@gmail.com>
2016-05-12 17:41:40 +0900

    Update database.js

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ module.exports = {
     'connection': {
         'host': 'tacademy.c9uxminr0qga.ap-northeast-1.rds.amazonaws.com',
         'user': 'admin',
-        'password': 'rkaehr8989',
+        'password': '***',
         'insecureAuth': 'true'
     },
 	'database': 'moyak',
