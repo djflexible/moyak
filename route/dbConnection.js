12519b8b338cfa0136dc702a3b5d654c99184eab
LIM DONG JOO <dongdongalcohol@gmail.com>
2016-05-14 00:23:03 +0900

    Update dbConnection.js

--- route/dbConnection.js
+++ route/dbConnection.js
@@ -4,7 +4,7 @@ var mysql = require('mysql');
 var dbConfig = {
    host: 'tacademy.c9uxminr0qga.ap-northeast-1.rds.amazonaws.com',
    user: 'admin',
-   password: 'rkaehr8989',
+   password: '***',
    //port: 3307,
    database: 'moyak',
    insecureAuth: true
