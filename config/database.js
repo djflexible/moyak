dc6e5990ee7499c53def63a8ef5b2c884d239f6a
LIM DONG JOO <dongdongalcohol@gmail.com>
2016-05-14 02:05:50 +0900

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 // config/database.js
 module.exports = {
     'connection': {
-        'host': 'tacademy.c9uxminr0qga.ap-northeast-1.rds.amazonaws.com',
+        'host': '***.c9uxminr0qga.ap-northeast-1.rds.amazonaws.com',
         'user': 'admin',
         'password': '***',
         'insecureAuth': 'true'
