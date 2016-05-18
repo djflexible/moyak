bb25cf8c0356bfa02b86b5404a79e834ca43d2da
LIM DONG JOO <dongdongalcohol@gmail.com>
2016-05-18 18:54:27 +0900

    Update loginroutes.js

--- route/loginroutes.js
+++ route/loginroutes.js
@@ -1,5 +1,3 @@
-// app/routes.js
-
 module.exports = function(app, passport) {
 
 	app.get('/', function(req, res) {
@@ -15,44 +13,43 @@ app.post('/login', function(req, res){
 	passport.authenticate('local-login', function(err, user, msg, statusCode){
 
 		if ( ! user ) {
-						 console.log('err 값 : ', err);
-						 console.log('user 값 : ', user);
-						 console.log('로그인 안됨2');
-		         //res.status(404).json({id:-1});
-		         res.status(404).end();
+				 console.log('err 값 : ', err);
+				 console.log('user 값 : ', user);
+				 console.log('로그인 안됨2');
+		         	//res.status(404).json({id:-1});
+		         	res.status(404).end();
 		         return;
 		      }
 		      // 세션에 기록
     req.logIn(user, function(err) {
-       if ( err ) {
-          res.status(404).json({msg:'Session Write Error'});
-          return;
-       }
-       console.log('로그인 됨');
-       res.json({id : user.id});//로그인 성공 반환값
-	});
- })(req);
-});
+				       if ( err ) {
+				          res.status(404).json({msg:'Session Write Error'});
+				          return;
+				       }
+				       console.log('로그인 됨');
+				       res.json({id : user.id});//로그인 성공 반환값
+				});
+ 			})(req);
+		});
 
 	app.get('/signup', function(req, res) {});
-
 	app.post('/signup', function(req, res){
 		passport.authenticate('local-signup', function(err, user, msg, statusCode){
-			if ( ! user ) {
-			         res.status(404).json({id:-1});
-			         return;
-			      }
+				if ( ! user ) {
+			         	res.status(404).json({id:-1});
+			         	return;
+					}
 			      // 세션에 기록
-	    req.logIn(user, function(err) {
-	       if ( err ) {
-	       		console.log('req.body', req.body);
-	          res.status(404).json({msg:'Session Write Error'});
-	          return;
-	       }
-	     res.json({id : user.id});
-			});
-	 	})(req);
-	});
+		 req.logIn(user, function(err) {
+			 if ( err ) {
+			       		console.log('req.body', req.body);
+			        	 res.status(404).json({msg:'Session Write Error'});
+			          	return;
+			 		}
+				 res.json({id : user.id});
+				});
+			})(req);
+		});
 
 	app.get('/profile', isLoggedIn, function(req, res) {
 		console.log('로그인 확인 페이지');
