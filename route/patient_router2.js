a75e58c608f4fe16013f716f793d8e86cfa87e76
LIM DONG JOO <dongdongalcohol@gmail.com>
2016-05-18 19:26:57 +0900

    Update patient_router2.js

--- route/patient_router2.js
+++ route/patient_router2.js
@@ -24,107 +24,107 @@ exports.editPatient = function(req, res){
 			pool.getConnection(function(err, conn){
 					var sql = 'UPDATE patient SET gender = ?, name = ?, birthyear = ?, profilePicture = ?  WHERE id = ?';
 					var patientData = {
-													//'phone': phone,
-													//'password' : password,
-													'gender' : gender,
-													'name' : name,
-													'birthyear' : birthyear,
-													'profilePicture' : profilePicture
-												};
+								//'phone': phone,
+								//'password' : password,
+								'gender' : gender,
+								'name' : name,
+								'birthyear' : birthyear,
+								'profilePicture' : profilePicture
+							};
 					conn.query(sql, [gender, name, birthyear, profilePicture, id], function(err, rows){
 						console.log('수정할 환자정보', patientData);	//환자 정보
 								if(err) {
-											console.log('err : ', err)
-									    res.statusCode = 404;
-									    res.end('EDIT FAIL');
-													}
+										console.log('err : ', err)
+										res.statusCode = 404;
+										 res.end('EDIT FAIL');
+									}
 									else {
 										res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
 										res.end(JSON.stringify(patientData));
-									}
-								conn.release();
-								});
-							});
-						}
-
-	//문의서 생성(직접 생성)
-	exports.addRequest = function(req, res){	//post 방식
-
-		var patientId = req.body.patientId;
-		var replace = req.body.replace;
-		var quick = req.body.quick;
-		var distance = req.body.distance;
-		var format = req.body.format;
-		var latitude = req.body.latitude;
-		var longitude = req.body.longitude;
-
-		console.log('리퀘스트에서 환자 아이디 출력 : ', patientId);
-		pool.getConnection(function(err, conn){		//dbConnect
-			console.log('로그1');
-			var requestdata = {
-						'patientId': patientId,
-						'replace' : replace,
-						'quick' : quick,
-						'distance' : distance,
-						'format' : format,
-						'latitude' : latitude,
-						'longitude' : longitude
-					};
-			var query = 'INSERT INTO request SET ?';
-			conn.query(query, requestdata, function(err, result){
-					if(err){
-							res.status(404).json({id:-1});
-							res.end('INSERT FAIL1');
-							return;
-							}
-					//res.status(200)
-					//res.status(200).json({id:1});
-					console.log('INSERT SUCCESS1');	//리퀘스트 저장 완료
-					console.log('result 값 : ', result);
-					console.log('requestId 값 : ', result.insertId);
-
-					async.each(req.body.pills, function(pill, callback) {
-						var patientId = req.body.patientId;
-							var pilldata = {
-									'requestId' : result.insertId,	//requestrId 추가
-									'name': pill.name,
-									'company' : pill.company,
-									'doseAmount' : pill.doseAmount,
-									'doseTimes' : pill.doseTimes,
-									'doseDays' : pill.doseDays
-								};
-								console.log('리퀘스트 아이디3 : ', req.body.requestId);
-								console.log('리퀘스트 아이디4 : ', req.body.id);
-								console.log('pilldata : ', pilldata);
-								console.log('중간 확인');
-
-								var query2 = 'INSERT INTO pill set ?';
-								conn.query(query2, pilldata, function(err, result){
-									console.log('쿼리 2 시작');
-										if(err){
-												callback(err);
-												res.end('INSERT FAIL2');
-											}
-										else{
-												console.log('INSERT SUCCESS2');
-												console.log('pilldata 확인: ', pilldata);
-												callback(null);
 										}
-									});//2번째 쿼리 종료
-								console.log('로그4');
-							}, function(err, result){
-						if(err){
-							console.log('ERR', err);
-								res.end('INSERT ERR');
+									conn.release();
+									});
+								});
 							}
-						else{
-							res.status(200);
-							res.end('INSERT SUCCESS2');
-						}
-					})//async종료
-				console.log('로그3');
-				conn.release();
-			});//1번째 쿼리 종료
-		console.log('로그2');
-		});
-	}
+
+//문의서 생성(직접 생성)
+exports.addRequest = function(req, res){	//post 방식
+
+	var patientId = req.body.patientId;
+	var replace = req.body.replace;
+	var quick = req.body.quick;
+	var distance = req.body.distance;
+	var format = req.body.format;
+	var latitude = req.body.latitude;
+	var longitude = req.body.longitude;
+
+	console.log('리퀘스트에서 환자 아이디 출력 : ', patientId);
+	pool.getConnection(function(err, conn){		//dbConnect
+		console.log('로그1');
+		var requestdata = {
+					'patientId': patientId,
+					'replace' : replace,
+					'quick' : quick,
+					'distance' : distance,
+					'format' : format,
+					'latitude' : latitude,
+					'longitude' : longitude
+				};
+		var query = 'INSERT INTO request SET ?';
+		conn.query(query, requestdata, function(err, result){
+				if(err){
+					res.status(404).json({id:-1});
+					res.end('INSERT FAIL1');
+					return;
+					}
+				//res.status(200)
+				//res.status(200).json({id:1});
+				console.log('INSERT SUCCESS1');	//리퀘스트 저장 완료
+				console.log('result 값 : ', result);
+				console.log('requestId 값 : ', result.insertId);
+
+				async.each(req.body.pills, function(pill, callback) {
+					var patientId = req.body.patientId;
+						var pilldata = {
+								'requestId' : result.insertId,	//requestrId 추가
+								'name': pill.name,
+								'company' : pill.company,
+								'doseAmount' : pill.doseAmount,
+								'doseTimes' : pill.doseTimes,
+								'doseDays' : pill.doseDays
+							};
+							console.log('리퀘스트 아이디3 : ', req.body.requestId);
+							console.log('리퀘스트 아이디4 : ', req.body.id);
+							console.log('pilldata : ', pilldata);
+							console.log('중간 확인');
+
+							var query2 = 'INSERT INTO pill set ?';
+							conn.query(query2, pilldata, function(err, result){
+								console.log('쿼리 2 시작');
+									if(err){
+										callback(err);
+										res.end('INSERT FAIL2');
+										}
+									else{
+										console.log('INSERT SUCCESS2');
+										console.log('pilldata 확인: ', pilldata);
+										callback(null);
+									}
+								});//2번째 쿼리 종료
+							console.log('로그4');
+							}, function(err, result){
+								if(err){
+									console.log('ERR', err);
+									res.end('INSERT ERR');
+									}
+								else{
+									res.status(200);
+									res.end('INSERT SUCCESS2');
+									}
+								})//async종료
+						console.log('로그3');
+						conn.release();
+							});//1번째 쿼리 종료
+					console.log('로그2');
+						});
+					}
